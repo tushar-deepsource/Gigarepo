beab80804338d5cf6410457eddced3ff5c7d6027
Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>
2022-09-16 17:34:29 +0530

    Update code.rb
    Signed-off-by: Tushar Sadhwani <86737547+tushar-deepsource@users.noreply.github.com>

--- ruby/code.rb
+++ ruby/code.rb
@@ -1,11 +1,11 @@
-# encoding: ascii
+# frozen_string_literal: true
 
 # Raises "bad ordering of magic comments"
 # frozen_string_literal: true
-# frozen_string_literal: true
+# encoding: ascii
 
 # Raises "use of deprecated BigDecimal.new"
-a = { 'hello' => 'world', 'testing' => BigDecimal(123.456, 3) }
+a = { 'hello' => 'world', 'testing' => BigDecimal.new(123.456, 3) }
 
 # Raises "`while`/`until` detected in `begin` block"
 begin
@@ -13,60 +13,47 @@ begin
 end while a == b
 
 # Raises "multiple comparison detected"
-x < y && y < z
-10 <= x && x <= 20
+x < y < z
+10 <= x <= 20
 
 # Raises "empty rescue block detected"
 begin
   bar
-rescue StandardError
+rescue
 end
 
 # Raises "redundant `else`-clause detected"
 if bar
+else
 end
 
 # Raises "unused method arguments detected"
-def some_method(_bar)
+def some_method(bar)
   puts 'Hello'
 end
 
-# Raises "unreachable code detected"
-def some_method
-  return
-  do_something
-end
-
-# Raises "top level return with argument detected"
-return 1
-
-# Raises "duplicate elsif block detected"
-if x == 1
-  do_something
-elsif x == 1
-  do_something_else
+	@@ -49,17 +48,16 @@ def some_method
 end
 
 # Raises "Deprecated way of initializing OpenSSL::Cipher and OpenSSL::Digest"
-OpenSSL::Cipher.new('aes-128-gcm')
+OpenSSL::Cipher::AES.new(128, :GCM)
 
 # Raises "put empty method definitions on a single line"
 # Also raises "multiple methods with same name in the same scope" as we have a
 # method with same name above
-def some_method; end
+def some_method
+end
 
 # Raises "Invalid annotation keyword format detected"
 def foo
-  # TODO: Replace this with bar
+  # TODO Replace this with bar
   do_something
 end
 
-# Raises "Use `Range#cover?` instead of `Range#include?`"
-(1..9).include?(5)
-
-my_hash = {}
+	@@ -70,6 +68,5 @@ def foo
 # Raises "Hash merging can be replaced by hash key assignment"
 my_hash.merge!('key': value)
+
 
 # Raises "Use `size` instead of `count`"
 [1, 2, 3].count
